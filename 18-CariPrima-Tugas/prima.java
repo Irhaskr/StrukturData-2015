38c71c90e9a6a7365ebba4533a902d1ef7230716
irhaskr <irhaskr@gmail.com>
2015-11-26 23:42:39 +0700

    Tugas

---
+++ 18-CariPrima-Tugas/prima.java
@@ -0,0 +1,31 @@
+/**
+ * Write a description of class prima here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class prima
+{   
+   //  int prima [] = new int[1000000];
+
+    /**
+     * Constructor for objects of class prime
+     */
+   public prima()
+   {   int angka = 100;
+       
+       for(int j=1;j<angka;j++){
+       int periksa=0;
+       for(int i=2;i<j;i++){
+         if((j%i)==0){
+           periksa=1;
+           break;
+         }
+       }
+       
+       if (periksa==0){
+           System.out.println(j);
+       }
+      }
+   }
+}
